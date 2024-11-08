cfd5e4f6ae928faf044115f694614452aaea570b
Caiox07 <160685805+Caiox07@users.noreply.github.com>
2024-11-08 01:24:07 +0000

    Codigo pronto, mas a tela ta branca

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [peso,setPeso]= useState(0);
+  const [Altura,setAltura]= useState(0);
+  const [imc,setImc]= useState(null);
+
+  const calcularImc=() =>{
+    var resultado=peso/(Altura*Altura);
+    setImc(resultado.toFixed(2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Aplicativo calculo IMC</Text>
-      <TextInput placeholder='peso' keyboardType='numeric'/>
+      <Text style={styles.titulo}>Aplicativo calculo IMC</Text>
+      
+      <TextInput 
+      placeholder='peso' 
+      keyboardType='numeric'
+      value={peso}
+      onChangeText={(peso) =>setPeso(peso)}
+      />
+      
+      <TextInput 
+      placeholder='Altura' 
+      keyboardType='numeric'
+      value={Altura}
+      onChangeText={(Altura)=>setAltura(Altura)}
+      />
 
-      <StatusBar style="auto" />
+      {imc && (
+      <text style={styles.titulo}>IMC:{imc}</text>
+      )}
+      <Button title='Calcular' onPress={()=>{calcularImc()} }/>
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -19,4 +47,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontSize: 25,
+    fontWeigth: 'bold'
+  }
 });
